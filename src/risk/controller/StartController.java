b56fe19d2d0a2f2acc3181dae3e8aa0604f062f5
PaigeWong <43482175+PaigeWong@users.noreply.github.com>
2018-11-08 00:17:33 -0500

    Update StartController.java

--- src/risk/controller/StartController.java
+++ src/risk/controller/StartController.java
@@ -1,7 +1,10 @@
 package risk.controller;
 
 public class StartController {
-	
+	/**
+	 * Main method to startup  controller.
+	 * @param args command line parameters
+	 */
 	public static void main(String[] args) {
 		GameController gameController = GameController.getInstance();
 		GUIController guiController = GUIController.getInstance();
