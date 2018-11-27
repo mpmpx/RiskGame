ef4f9b23129460083c6d32d74862b4977eae9774
PaigeWong <43482175+PaigeWong@users.noreply.github.com>
2018-11-26 22:48:35 -0500

    Update Strategy.java

--- src/risk/game/strategy/Strategy.java
+++ src/risk/game/strategy/Strategy.java
@@ -4,11 +4,16 @@ package risk.game.strategy;
  * The strategy interface;
  */
 public interface Strategy {
-	
+	/**
+	 * This documents "Behavior"
+	 */
 	public enum Behavior {
 		HUMAN, AGGRESSIVE, BENEVOLENT, RANDOM, CHEATER
 	};
 	
+	/**
+  	* Method to get its behavior
+  	*/ 	
 	Behavior getBehavior();
 	void startup();
 	void reinforce();
